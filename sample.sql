dbc4ee947693f184d8cb1664bf5b376967a45817
Addy$ <62867736+askforaritra@users.noreply.github.com>
2024-09-28 02:11:55 +0530

    Create sample.sql

---
+++ sample.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Super_Store (
+    Row_ID INT,
+    Order_Date DATE,
+    Ship_Date DATE,
+    Ship_Mode VARCHAR(50),
+    Customer_ID VARCHAR(50),
+    Customer_Name VARCHAR(50),
+    Segment VARCHAR(50),
+    Country VARCHAR(50),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    Postal_Code   
+ VARCHAR(50),
+    Region VARCHAR(50),
+    Product_ID VARCHAR(250),
+    Category VARCHAR(250),
+    Sub_Category VARCHAR(250),
+    Product_Name VARCHAR(250),
+    Sales INT
+);
